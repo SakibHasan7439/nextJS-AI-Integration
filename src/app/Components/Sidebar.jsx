b52f7cae09a9eedd0339e0a83526d875a630220f
sakibHasan7439 <sakibhasanb120@gmail.com>
2025-03-07 00:54:38 +0600

    all section completed

--- src/app/Components/Sidebar.jsx
+++ src/app/Components/Sidebar.jsx
@@ -5,7 +5,7 @@ const Sidebar = () => {
   return (
     <aside className="w-64 hidden bg-blue-900 fixed z-10 top-[74px] text-white min-h-screen p-4 lg:flex lg:flex-col">
       <h1 className="text-2xl font-bold mb-24 overflow-y-auto ">EchoGPT</h1>
-      <nav className="flex-1">
+      <nav>
         <Link href="chatHistory" className="flex mb-6 items-center hover:text-gray-300">
           <Image src={'/icons8-chat-48.png'} className="mr-2" alt="history icon" width={20} height={20}/> History
         </Link>
@@ -18,7 +18,7 @@ const Sidebar = () => {
           <Image src={'/icons8-test-48.png'} className="mr-2" alt="history icon" width={20} height={20}/> AI Tasks
         </li>
       </nav>
-      <div className="bg-blue-700 p-4 rounded-lg shadow mt-auto">
+      <div className="bg-blue-700 p-4 rounded-lg shadow mt-64">
         <p className="text-sm text-gray-200">Unlock pro features</p>
         <button className="mt-2 bg-yellow-400 text-blue-900 px-2 py-1 rounded">
             Upgrade to Pro
